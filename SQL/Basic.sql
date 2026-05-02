8132382e871c5ad4719049501283c1b44d47f0f0
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-05-02 23:00:17 +0530

    creating new database

--- SQL/Basic.sql
+++ SQL/Basic.sql
@@ -1,3 +1,4 @@
+#creating_Database
 CREATE DATABASE Collage;
 USE Collage;
 CREATE TABLE student(
@@ -11,3 +12,17 @@ AGE INT
 insert into student values
 (1,"shrajal","abc@gmail.com",123,20),
 (2,"shakti","abc2@gmail.com",124,21);
+
+
+
+#creating_database
+CREATE DATABASE Instagram;
+USE Instagram;
+CREATE TABLE Users(
+    ID INT,
+    EMAIL VARCHAR(100),
+    AGE INT,
+    USER_NAME VARCHAR(100),
+    FOLLOWERS INT,
+    FOLLOWING INT
+);
