05dcb3a54e2732addc152e1a74f19f73726d2157
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-05-06 22:33:35 +0530

    Roll-Back

--- SQL/Adv.sql
+++ SQL/Adv.sql
@@ -6,9 +6,16 @@ name varchar(50),
 balance decimal(10,2)
 );
 
-
+-- Transaction
 START TRANSACTION;
 update accounts set balance = balance - 50 where id = 1;
 update accounts set balance = balance +50 where id = 2;
 
 COMMIT;
+
+-- ROLL BACK
+start transaction;
+update accounts set balance = balance-100 where id = 1;
+commit;
+update accounts set balance = balance + 100 where id = 3;
+rollback;
