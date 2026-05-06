ab05fb56cf9ced6ab0816b08e9fe818553a98a4f
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-05-06 22:05:43 +0530

    insert values into  table

--- SQL/assianment.sql
+++ SQL/assianment.sql
@@ -102,3 +102,17 @@ DEPARTMENT VARCHAR(50),
 SALARY INT ,
 HIREDATE DATE
 );
+
+use assianment;
+INSERT INTO EMPLOYEE
+VALUES
+(101,"Alice","Johnson","IT",6500,'2020-03-15'),
+(102,"Mark","Rivera","HR",4800,'2020-07-22'),
+(103,"Sophia","Lee","Finance",7200,'2021-01-10'),
+(104,"Daniel","Kim","IT",5800,'2018-11-05'),
+(105,"Emma","Brown","Marketing",5300,'2022-04-18'),
+(106,"Liam","Patel","Finance",6900,'2020-09-29'),
+(107,"Olivia","Garcia","HR",4600,'2017-06-30'),
+(108,"Noah","Thompson","IT",7500,'2023-02-12'),
+(109,"Ava","Martinez","marketing",5100,'2019-12-02'),
+(110,"Ethan","Davis","Finance",8000,'2016-05-14');
