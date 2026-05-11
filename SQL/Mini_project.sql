80d6468e17a616223f489350f937dd2268a2fe73
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-05-11 23:15:09 +0530

    Create table customers

--- SQL/Mini_project.sql
+++ SQL/Mini_project.sql
@@ -1,2 +1,9 @@
 CREATE DATABASE ecommerce_db;
 use ecommerce_db;
+
+CREATE TABLE customers(
+    customer_id int primary key auto_increment,
+    name varchar(50),
+    email varchar(100) unique,
+    city varchar(50)
+);
